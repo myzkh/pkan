2912979d9b83ba2f59af3739431dfa66d8a9cc4b
miyazaki.h <hidehiko.miyazaki@gitobi.com>
2018-02-23 16:57:38 +0900

    firstcommit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCFskyWz5vxZzdThl7OhgNFSVXlxxFJA7o",
+    authDomain: "pkan-8880a.firebaseapp.com",
+    databaseURL: "https://pkan-8880a.firebaseio.com",
+    projectId: "pkan-8880a",
+    storageBucket: "pkan-8880a.appspot.com",
+    messagingSenderId: "231485158025"
+}
